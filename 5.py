36a7a80894e083c2dfcb6fa2e2b1b85184bfeeeb
shreya_pr <46067084+shreyapr6@users.noreply.github.com>
2019-06-05 11:46:24 +0530

    Deleting old files

--- 5.py
+++
@@ -1,7 +0,0 @@
-#Write a Python program to check if a string is numeric.
-
-st = input("Enter the string\n")
-if st.isalpha() is False:
-	print("True")
-else:
-	print("False")
--- io_1.py
+++
@@ -1,9 +0,0 @@
-#WAP that accepts a sequence of comma separated numbers from user 
-#and generates a list and tuple with those numbers
-
-
-nums = input("Enter the numbers\n").split(",")
-nums = [int(n) for n in nums]
-print(nums)
-
-print(tuple(nums))
